ff6a3d7d7a7d6bf07412a36ef02f8885e83b993d
Max Schaefer <schaefm2@wwu.edu>
2025-08-19 22:14:25 -0700

    Commit before I reset my computer

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -2,16 +2,17 @@ import { useState } from "react";
 import ProjSideBar from "../components/Project/ProjSideBar";
 import Project from "../components/Project/Project";
 import fallingSandImg from "../assets/falling_sand.png";
+import chaosImg from "../assets/chaos-screenshot.png";
 const Projects = () => {
   const [displayedProject, setDisplayedProject] = useState({
     name: "Falling Sand",
-    description: "This is a description of my project.",
+    description: "Left click and drag to create sand particles!",
     repoLink: "https://schaefm2.github.io/FallingSand/",
     imgSrc: fallingSandImg,
   });
   const projectNames = {
     "School Projects": [
-      "Chaos Engineering",
+      "Chaos Engineering Senior Project",
       "Drone Tracking",
       "Zombie Shooter Game",
     ],
@@ -21,14 +22,14 @@ const Projects = () => {
   const projects = [
     {
       name: "Falling Sand",
-      description: "This is a description of my project.",
+      description: "Left click and drag to create sand particles!",
       repoLink: "https://schaefm2.github.io/FallingSand/",
       imgSrc: fallingSandImg,
     },
     {
       name: "Drone Tracking",
-      description: "This is a description of my project.",
-      repoLink: "https://schaefm2.github.io/FallingSand/",
+      description:
+        "My team and I created a drone traching system using crazyflie with aruco tags. We were able to successfully track a target and steer the drone to it using PID control.",
       imgSrc: fallingSandImg,
     },
     {
@@ -38,10 +39,10 @@ const Projects = () => {
       imgSrc: fallingSandImg,
     },
     {
-      name: "Chaos Engineering",
-      description: "This is a description of my project.",
-      repoLink: "https://schaefm2.github.io/FallingSand/",
-      imgSrc: fallingSandImg,
+      name: "Chaos Engineering Senior Project",
+      description:
+        "Me and 3 other students created a chaos tool for the Western Washington University Cyber Range. \n This project is currently available on the cyber range. \n Download the presentation to learn more.",
+      imgSrc: chaosImg,
     },
     {
       name: "Visualizing Merge Sort",
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,21 +1,36 @@
-import React from "react";
 import "./Project.css";
 
 const Project = ({ project }) => {
+  const docFiles = [
+    {
+      uri: "my-website/assets/chaosPresentation.ppt",
+    },
+  ];
   return (
     <div className="project-container">
-      <label className="project-label">{project.name}</label>
-      <a
-        href={project.repoLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="project-link"
-      >
-        See on GitHub
-      </a>
+      <h3 className="project-header">{project.name}</h3>
+      {project.repoLink && (
+        <a
+          href={project.repoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="project-link"
+        >
+          See on GitHub
+        </a>
+      )}
       <p className="project-description">{project.description}</p>
       {project.imgSrc && (
         <img src={project.imgSrc} alt="Project" className="project-image" />
+      )}
+      {project.name === "Chaos Engineering Senior Project" && (
+        <a
+          className="project-a"
+          href="public/assets/chaosPresentation.ppt"
+          download
+        >
+          Download Presentation
+        </a>
       )}
     </div>
   );
